e8c0f712c40a6e05ce4ad42db47d58d6a717a5fa
Matej Kupciha <kupciham@hotmail.com>
2020-04-24 11:40:01 +0200

    Remove trailing spaces

--- h264decoder.hpp
+++ h264decoder.hpp
@@ -1,19 +1,19 @@
 #pragma once
 /*
-This h264 decoder class  is just a thin wrapper around libav 
-functions to decode h264 videos. It would have been easy to use 
-libav directly in   the python module code but I like to keep these 
+This h264 decoder class  is just a thin wrapper around libav
+functions to decode h264 videos. It would have been easy to use
+libav directly in   the python module code but I like to keep these
 things separate.
- 
+
 It is mostly based on roxlu's code. See
 http://roxlu.com/2014/039/decoding-h264-and-yuv420p-playback
 
 However, in contrast to roxlu's code the color space conversion is
 done by libav functions - so on the CPU, I suppose.
 
-Most functions/members throw exceptions. This way, error states are 
-conveniently forwarded to python via the exception translation 
-mechanisms of boost::python.  
+Most functions/members throw exceptions. This way, error states are
+conveniently forwarded to python via the exception translation
+mechanisms of boost::python.
 */
 
 // for ssize_t (signed int type as large as pointer type)
@@ -55,19 +55,19 @@ class H264Decoder
   AVFrame               *frame;
   AVCodec               *codec;
   AVCodecParserContext  *parser;
-  /* In the documentation example on the github master branch, the 
-packet is put on the heap. This is done here to store the pointers 
-to the encoded data, which must be kept around  between calls to 
-parse- and decode frame. In release 11 it is put on the stack, too. 
+  /* In the documentation example on the github master branch, the
+packet is put on the heap. This is done here to store the pointers
+to the encoded data, which must be kept around  between calls to
+parse- and decode frame. In release 11 it is put on the stack, too.
   */
   AVPacket              *pkt;
 public:
   H264Decoder();
   ~H264Decoder();
-  /* First, parse a continuous data stream, dividing it into 
-packets. When there is enough data to form a new frame, decode 
-the data and return the frame. parse returns the number 
-of consumed bytes of the input stream. It stops consuming 
+  /* First, parse a continuous data stream, dividing it into
+packets. When there is enough data to form a new frame, decode
+the data and return the frame. parse returns the number
+of consumed bytes of the input stream. It stops consuming
 bytes at frame boundaries.
   */
   std::size_t parse(const unsigned char* in_data, std::size_t in_size);
@@ -80,16 +80,16 @@ class ConverterRGB24
 {
   SwsContext *context;
   AVFrame *framergb;
-  
+
 public:
   ConverterRGB24();
   ~ConverterRGB24();
-   
-  /*  Returns, given a width and height, 
+
+  /*  Returns, given a width and height,
       how many bytes the frame buffer is going to need. */
   int predict_size(int w, int h);
-  /*  Given a decoded frame, convert it to RGB format and fill 
-out_rgb with the result. Returns a AVFrame structure holding 
+  /*  Given a decoded frame, convert it to RGB format and fill
+out_rgb with the result. Returns a AVFrame structure holding
 additional information about the RGB frame, such as the number of
 bytes in a row and so on. */
   const AVFrame& convert(const AVFrame &frame, unsigned char* out_rgb);
